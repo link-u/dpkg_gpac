ab5a22d7a2e01cdbf6696cccb3adbddb31aecee4
psi <ryo.hirafuji@link-u.co.jp>
2020-06-05 20:25:13 +0900

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ set -eux
 SCRIPT_PATH=$(readlink -f $(cd $(dirname $0) && pwd))
 cd ${SCRIPT_PATH}
 
+apt update
 mk-build-deps --install --remove \
               --tool='apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes' \
               gpac/debian/control
